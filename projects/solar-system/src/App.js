f52143a8c887ad181994cdc1276f7785df753bad
Philip Lages <psilvaelages@gmail.com>
2022-07-21 14:34:40 -0300

    Update to render on screen

--- projects/solar-system/src/App.js
+++ projects/solar-system/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
-import './App.css';
+import '../App.css';
 
 class App extends Component {
   render() {
